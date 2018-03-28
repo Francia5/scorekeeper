4ae6d663a0fbfd5276e1c5f7ea4c73cb1e865aa0
francia5 <francia.va.or@gmail.com>
2018-03-28 11:15:57 -0600

    En este commit se agregan 3 funciones en el JS para lograr la funcionalidad del problema a resolver.

--- js/app.js
+++ js/app.js
@@ -1,1 +1,33 @@
-console.log("hola mundo");
+var scores = {}; // Va a llevar el registro de los jugadores
+console.log(scores);
+//
+function cratePlayer(object, name) {
+  object[name.toLowerCase()] = 0;
+  console.log(object);
+}
+
+function addPoints(name, points) {
+  scores[name.toLowerCase()] += points;
+  console.log(scores);
+}
+
+function printAllPoints() {
+  var arrayPlayers = Object.keys(scores);
+  var result = "";
+  for(var k = 0; k < arrayPlayers.length; k++) {
+    if(k === arrayPlayers.length - 1) { //Se verifica que estemos en el último index
+      result += arrayPlayers[k] + ": " + scores[arrayPlayers[k]];//Si es el último índice no se deja la coma
+    } else {
+      result += arrayPlayers[k] + ": " + scores[arrayPlayers[k]] + ", ";//Si no es el último índice se deja la coma de separación
+    }
+  }
+  return result;
+}
+
+cratePlayer(scores, "Ana");
+addPoints("ana", 20);
+addPoints("ana", 40);
+cratePlayer(scores, "Silvana");
+addPoints("Silvana", 50);
+printAllPoints();
+console.log(printAllPoints());
